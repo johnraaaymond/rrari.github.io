35aa82cad04cd6ccc94f6e99370fec6e036e3607
johnraaaymond <126550164+johnraaaymond@users.noreply.github.com>
2024-08-13 15:30:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -110,4 +110,8 @@ function typeWriterEffect() {
 // Initialize the typewriter effect
 typeWriterEffect();
 
+// This adds some nice ellipsis to the description:
+document.querySelectorAll(".projcard-description").forEach(function(box) {
+    $clamp(box, {clamp: 6});
+  });
 
